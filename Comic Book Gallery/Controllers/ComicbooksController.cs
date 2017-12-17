352562c6da4efcde3f97b838f2e0a020dab00577
Kenyatta Payne <KenyattaPayne@outlook.com>
2017-12-17 14:59:29 -0600

    Added Detail Action Method

---
+++ Comic Book Gallery/Controllers/ComicbooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comic_Book_Gallery.Controllers
+{
+    public class ComicbooksController : Controller
+    {
+
+        public string Detail()
+        {
+
+            return "Hello from the comics book controller";
+
+        }
+    }
+
+}
