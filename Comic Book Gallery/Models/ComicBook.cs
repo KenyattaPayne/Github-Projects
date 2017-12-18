84559302b36e20fe634f7bf977959cab571fa7c3
Kenyatta Payne <KenyattaPayne@outlook.com>
2017-12-18 11:06:51 -0600

    Updated ComicBook.cs File

--- Comic Book Gallery/Models/ComicBook.cs
+++ Comic Book Gallery/Models/ComicBook.cs
@@ -14,6 +14,24 @@ namespace Comic_Book_Gallery.Models
         public string DescriptionHtml { get; set; }
         public Artists[] Artist { get; set; }
         public bool Favorite { get; set; }
-  
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+            
+        }
+        // series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg" ;
+            }
+        }
+
     }
 }
