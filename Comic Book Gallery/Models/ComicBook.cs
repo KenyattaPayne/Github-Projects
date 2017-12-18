b750c3e90ebff392c8b9cc7091d205f7059c60a3
Kenyatta Payne <KenyattaPayne@outlook.com>
2017-12-18 04:43:43 -0600

    Added Comic Book and Artists Models.

---
+++ Comic Book Gallery/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic_Book_Gallery.Models
+{
+    public class ComicBook
+    {
+
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artists[] Artist { get; set; }
+        public bool Favorite { get; set; }
+  
+    }
+}
